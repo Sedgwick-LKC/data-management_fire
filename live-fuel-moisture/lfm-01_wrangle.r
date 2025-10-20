8828c7b1a7d60fc1b2e9054454d5936fb8818c8b
njlyon0 <nicholasjlyon@gmail.com>
2025-10-20 11:23:35 -0400

    feat (lfm wrangle): removing download code--now lives in standalone download script

--- live-fuel-moisture/lfm-01_wrangle.r
+++ live-fuel-moisture/lfm-01_wrangle.r
@@ -15,32 +15,14 @@ source("00_setup.r")
 rm(list = ls()); gc()
 
 ## ----------------------------- ##
-# Download Data ----
-## ----------------------------- ##
-
-# For the code to talk to Drive, you need to tell R who you are (in Google)
-## Work through the following tutorial to do so
-### https://lter.github.io/scicomp/tutorial_googledrive-pkg.html
-## Alternatively, see the help file for the following function:
-### `?googledrive::drive_auth`
-
-# Identify the relevant file in Google Drive
-lfm_drive <- googledrive::drive_ls(path = googledrive::as_id("https://drive.google.com/drive/folders/1EOrKk39IppCX9gQ5KWlpO890XGpcgwzg")) %>% 
-  dplyr::filter(stringr::str_detect(string = name, pattern = "Live_fuel_moisture"))
-
-# Did that work?
-## It did if you see the name of an Excel file when you run the next line
-lfm_drive
-
-# Identify the file path we want locally
-lfm_path <- file.path("data", "raw", lfm_drive$name)
-
-# Download this file
-googledrive::drive_download(file = lfm_drive$id, overwrite = T, path = lfm_path)
-
-## ----------------------------- ##
 # Load Data ----
 ## ----------------------------- ##
+
+# Identify the most up-to-date file name
+lfm_name <- "Live_fuel_moisture_data_sheet_103023.xlsx"
+
+# Identify the file path we want locally
+lfm_path <- file.path("data", "raw", lfm_name)
 
 # What sheets are in the data?
 (lfm_tabs <- readxl::excel_sheets(path = lfm_path))
